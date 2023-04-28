eccb147fa74beb3d17f17e242cf7e3fa0a079fe9
chengpatrick <qq240766532@gmail.com>
2023-04-28 15:34:39 -0400

    added custom inspector button and seed

--- Assets/PerlinNoise/Script/PerlinGenerator.cs
+++ Assets/PerlinNoise/Script/PerlinGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PerlinGenerator : MonoBehaviour
 {
     Dictionary<int, GameObject> tileset;
+
     [Header("Tile Prefabs")]
     public GameObject prefab_grass;
     public GameObject prefab_earth;
@@ -14,25 +15,62 @@ public class PerlinGenerator : MonoBehaviour
     Dictionary<int, GameObject> tile_group;
 
     [Header("Map Size")]
-    public int width=16, height=9;
+    public int width = 16;
+    public int height=9;
+
+    [Header("Generate Setting")]
+    public bool use_seed = false;
     public float magnification = 7.0f;
     public int x_offset=0, y_offset=0;
+    public bool random_seed = false;
+    public int seed = 0;
 
     List<List<int>> noise_grid=new List<List<int>>();
+    // in scene grid
     List<List<GameObject>> tile_grid=new List<List<GameObject>>();
 
     // Start is called before the first frame update
     void Start()
     {
-        CreateTileSet();
-        CreateTileGroup();
-        GenerateMap();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGenerate()
+    {
+        CreateTileSet();
+        CreateTileGroup();
+        GenerateMap();
+    }
+
+    public void ClearScene()
+    {
+        foreach (List<GameObject> list in tile_grid)
+        {
+            foreach(GameObject obj in list)
+            {
+                DestroyImmediate(obj);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            GameObject.DestroyImmediate(child.gameObject);
+        }
+        foreach (Transform child in transform)
+        {
+            GameObject.DestroyImmediate(child.gameObject);
+        }
+        foreach (Transform child in transform)
+        {
+            GameObject.DestroyImmediate(child.gameObject);
+        }
+        tile_grid = new List<List<GameObject>>();
     }
     
     void CreateTileSet()
@@ -54,12 +92,20 @@ public class PerlinGenerator : MonoBehaviour
             group.transform.localPosition=new Vector3(0,0,0);
             tile_group.Add(pair.Key, group);
         }
-        
     }
 
     void GenerateMap()
     {
-        for(int x=0;x<width;x++)
+        if (use_seed)
+        {
+            if (random_seed)
+            {
+                GetRandomSeed();
+            }
+            GetSeedValue();
+        }
+
+        for (int x=0;x<width;x++)
         {
             noise_grid.Add(new List<int>());
             tile_grid.Add(new List<GameObject>());
@@ -75,6 +121,7 @@ public class PerlinGenerator : MonoBehaviour
 
     int GetIdByPerlin(int x, int y)
     {
+        
         float raw_perlin=Mathf.PerlinNoise((x-x_offset)/magnification,(y-y_offset)/magnification);
         float clamp_perlin=Mathf.Clamp(raw_perlin,0.0f,1.0f);
         float scale_perlin=clamp_perlin* tileset.Count;
@@ -83,7 +130,6 @@ public class PerlinGenerator : MonoBehaviour
             scale_perlin = 3;
         }
             
-
         return Mathf.FloorToInt(scale_perlin);
     }
 
@@ -93,9 +139,25 @@ public class PerlinGenerator : MonoBehaviour
         GameObject group = tile_group[id];
         GameObject tile=Instantiate(tile_prefab,group.transform);
 
-        tile.name=string.Format("tile_x[0]_y[1]",x,y);
+        tile.name=string.Format("tile_x[{0}]_y[{1}]",x,y);
         tile.transform.localPosition=new Vector3(x,0,y);
 
         tile_grid[x].Add(tile);
+    }
+
+    void GetRandomSeed()
+    {
+        seed = Random.Range(999, 9999);
+    }
+
+    void GetSeedValue()
+    {
+
+        float pi=Mathf.PI*seed;
+        int pi_int = (int)pi;
+        string pi_string = pi_int.ToString();
+
+        x_offset = pi_string[pi_string.Length-1]*pi_string[0]+ pi_string[pi_string.Length - 1] * pi_string[1];
+        y_offset = pi_string[pi_string.Length - 2] * pi_string[0] + pi_string[pi_string.Length - 1] * pi_string[1];
     }
 }
